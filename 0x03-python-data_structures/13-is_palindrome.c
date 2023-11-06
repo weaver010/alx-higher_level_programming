c9c8bb8399e9968468d8067461e023e64586cc89
weaver010 <abdelrahmannagaty839@gmail.com>
2023-11-06 14:42:28 -0800

    hi

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ * chpalindrome - check
+ * @h: h
+ * @a: a
+ * Return: 1
+ */
+int chpalindrome(listint_t **h, listint_t *a)
+{
+int r;
+if (a != NULL)
+{
+r = palindrome(h, a->next);
+if (r != 0)
+{
+r = (a->n == (*h)->n);
+*h  = (*h)->next;
+return (r);
+}
+return (0);
+}
+return (1);
+}
+
+/**
+ * is_palindrome - checks
+ * @head: head
+ * Return: 1
+ */
+int is_palindrome(listint_t **head)
+{
+if (head == NULL)
+{
+return (0);
+}
+return (chpalindrome(head, *head));
+}}
