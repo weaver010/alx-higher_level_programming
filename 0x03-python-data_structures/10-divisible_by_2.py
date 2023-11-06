e6a3239c50068709ccb4f9c96d04ee2b539432b1
weaver010 <abdelrahmannagaty839@gmail.com>
2023-11-06 13:05:21 -0800

    hi

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    s=[]
+    for i in my_list:
+        if i % 2 ==0:
+            s.append(True)
+        else:
+            s.append(False)
+    return s
