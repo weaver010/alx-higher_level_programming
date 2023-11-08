858bade5ed3209393732044b913190176d24026d
weaver010 <abdelrahmannagaty839@gmail.com>
2023-11-08 14:10:08 -0800

    hi

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    av = 0
+    al= 0
+    for i in my_list:
+        av += i[0] * i[1]
+        al += i[1]
+    return float(av / al)
