4fa7d7a830196a7e1d6e1c7f4550f28b43dc1d71
weaver010 <abdelrahmannagaty839@gmail.com>
2023-11-20 13:03:15 -0800

    hi

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+    except BaseException as a:
+        res = None
+        print("Exception: {}".format(a), file=sys.stderr)
+    finally:
+        return res
