802823d939fb2ff964e4bc15ae849875d1ff1563
weaver010 <abdelrahmannagaty839@gmail.com>
2023-11-21 12:59:04 -0800

    hi

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """
+    Attributes:
+        __size: size
+
+    """
+    def __init__(self, size):
+        """
+        Args:
+            size: size
+
+        Returns: None
+        """
+        self.__size = size
