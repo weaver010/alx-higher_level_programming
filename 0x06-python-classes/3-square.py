dc9685481742f50b233a4e6a6059abb64f479c84
weaver010 <abdelrahmannagaty839@gmail.com>
2023-11-21 13:08:14 -0800

    hi

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """
+    Attributes:
+        __size: size
+    """
+    def __init__(self, size=0):
+        """
+        Args:
+            size : size
+
+        Returns:
+            None
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        """
+        Returns: area
+        """
+        return (self.__size) * (self.__size)
