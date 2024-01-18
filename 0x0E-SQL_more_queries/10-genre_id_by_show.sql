6d504f81f471fd3aeb88e87c97b31072e1f3e8ef
weaver010 <abdelrahmannagaty839@gmail.com>
2024-01-18 13:42:01 -0800

    hi

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
---  a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT s.title AS title, g.genre_id AS genre_id
-FROM tv_shows b INNER JOIN tv_show_genres e
-ON b.id = e.show_id
-ORDER BY b.title, e.genre_id;
+ FROM tv_shows AS b 
+	INNER JOIN tv_show_genres AS e
+	ON b.id = e.show_id
+ ORDER BY b.title, e.genre_id;
