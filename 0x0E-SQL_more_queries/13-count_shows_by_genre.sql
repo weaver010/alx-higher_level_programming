a3b97231b2649fa29b0e70b84e452c82639c84eb
weaver010 <abdelrahmannagaty839@gmail.com>
2024-01-18 13:57:10 -0800

    hi

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,8 @@
-SELECT g.name AS genre, COUNT(gs.genre_id) AS number_of_shows
-FROM tv_shows s INNER JOIN tv_show_genres gs
-ON s.id = gs.show_id
+-- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT g.name AS name
+FROM tv_shows s INNER JOIN tv_show_genres sg
+ON s.id = sg.show_id
 INNER JOIN tv_genres g
-ON gs.genre_id = g.id
-GROUP BY g.name
-ORDER BY COUNT(gs.genre_id) desc;
+ON sg.genre_id = g.id
+WHERE s.title = 'Dexter'
+ORDER BY g.name;
