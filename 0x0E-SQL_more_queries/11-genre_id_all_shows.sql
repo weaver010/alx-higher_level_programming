163d92dd25b106f1bfd711c678bc94a053f02fe7
weaver010 <abdelrahmannagaty839@gmail.com>
2024-01-18 13:44:56 -0800

    hi

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- a script that lists all shows contained in the database hbtn_0d_tvshows.
 SELECT s.title AS title, g.genre_id AS genre_id
-FROM tv_shows AS r LEFT JOIN tv_show_genres t
-	ON r.id = t.show_id
-ORDER BY r.title, t.genre_id;
+FROM tv_shows AS s LEFT JOIN tv_show_genres g
+	ON s.id = g.show_id
+ORDER BY s.title, g.genre_id;
