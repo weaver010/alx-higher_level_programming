b92f041af6d7bd42bac42a40e4e04f931d5397bb
weaver010 <abdelrahmannagaty839@gmail.com>
2024-02-18 12:44:09 -0800

    hi

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that lists all State objects from the database hbtn_0e_6_usa"""
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
 
 if __name__ == "__main__":
 
@@ -8,9 +8,8 @@ if __name__ == "__main__":
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
 
-    s = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
+    s = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(s)
 
     c = Session(s)
