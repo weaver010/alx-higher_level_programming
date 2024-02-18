07a7597d240ce906c91633578ead30de7fbf7e73
weaver010 <abdelrahmannagaty839@gmail.com>
2024-02-18 12:40:45 -0800

    hi

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -18,6 +18,6 @@ if __name__ == '__main__':
                 WHERE states.name = %s\
                 ORDER BY cities.id ASC", (sys.argv[4],))
     s = c.fetchall()
-    print(", ".join([row[0] for row in rows]))
+    print(", ".join([row[0] for row in s]))
     c.close()
     db.close()
