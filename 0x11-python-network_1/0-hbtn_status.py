9cec339631fdfc4df4d0a5fb86adfc7b7d0f07f4
weaver010 <abdelrahmannagaty839@gmail.com>
2024-03-01 10:52:48 -0800

    hi

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """script that fetches https://alx-intranet.hbtn.io/status"""
+
+
 import urllib.request
 
-
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as
-    response:
-        f = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(f)))
-        print("\t- content: {}".format(f))
-        print("\t- utf8 content: {}".format(f.decode("UTF-8")))
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    f = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(f)))
+    print("\t- content: {}".format(f))
+    print("\t- utf8 content: {}".format(f.decode('utf-8')))
