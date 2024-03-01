4bdefd6e49ac6d69340962b5787eb1ad2fb5a95a
weaver010 <abdelrahmannagaty839@gmail.com>
2024-03-01 11:53:18 -0800

    hi

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,12 +2,13 @@
 """
  script that takes your GitHub credentials
 """
+
+import sys
 import requests
 from requests.auth import HTTPBasicAuth
-from sys import argv
 
-if __name__ == '__main__':
-    s = 'https://api.github.com/users/{}'.format(argv[1])
-    d = requests.get(s,
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(d.json().get('id'))
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    f = requests.get("https://api.github.com/user", auth=auth)
+    print(f.json().get("id"))
